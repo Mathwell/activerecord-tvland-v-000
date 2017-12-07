11ff86765b62a75303ef7e7f43f428cb4ce09890
Olena Ageyeva <eageeva@hotmail.com>
2017-12-07 00:41:53 +0000

    automatically backed up by learn

--- db/migrate/004_add_actor.rb
+++ db/migrate/004_add_actor.rb
@@ -3,9 +3,9 @@ class CreateActors < ActiveRecord::Migration
     create_table :actors do |t|
       #primary key of :id is created for us!
       t.string :last_name
-      t.string :first_name 
+      t.string :first_name
       t.integer :show_id
-      t.integer :character_id     
+      t.integer :character_id
     end
   end
 end
